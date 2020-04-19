58fe2ba0106419a7dd3761dd9a868ac5277f3e61
yhs <mvmaniaz@gmail.com>
2020-04-19 23:01:05 +0900

    [추가] redux 완료

---
+++ 01-redux/src/index.js
@@ -0,0 +1,40 @@
+const {createStore} = require('redux');
+
+const reducer = (prevState, action) => {
+  switch (action.type) {
+    case 'CHANGE_COMP_A':
+      return {
+        ...prevState,
+        compA: action.data
+      };
+    case 'CHANGE_COMP_B':
+      return {
+        ...prevState,
+        compB: action.data
+      };
+    case 'CHANGE_COMP_C':
+      return {
+        ...prevState,
+        compC: action.data
+      };
+    default:
+      return prevState;
+  }
+};
+
+const initialState = {
+  compA: 'a',
+  compB: 10,
+  compC: null
+};
+
+// store
+const store = createStore(reducer, initialState);
+store.subscribe(() => console.log(store.getState()));
+
+// action
+const changeComp = (type, data) => ({type, data});
+
+store.dispatch(changeComp('CHANGE_COMP_A', 'b'));
+store.dispatch(changeComp('CHANGE_COMP_B', 11));
+store.dispatch({type: 'CHANGE_COMP_C', data: undefined});
---
+++ README.md
@@ -0,0 +1,31 @@
+# Basic Redux Mobx
+
+### 1. Redux vs MobX (둘 다 배우자!) 강의 실습 예제 기반
+
+* [Redux vs MobX (둘 다 배우자!)](https://www.inflearn.com/course/redux-mobx-%EC%83%81%ED%83%9C%EA%B4%80%EB%A6%AC-%EB%8F%84%EA%B5%AC# "Redux vs MobX (둘 다 배우자!)") 참고
+
+### 2. 차이점
+
+* ESLint & Prettier 설정 추가
+
+### 3. TODO
+
+*
+
+### 4. Setting
+
+* react & redux  
+
+    ``` javascript
+    npm i react react-dom react-redux
+    npm i redux
+    ```
+
+* eslint & prettier
+  
+    ``` javascript
+    npm i -D eslint
+    npm i -D eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks
+    npm i -D prettier eslint-config-prettier eslint-plugin-prettier
+    npm i -D babel-eslint
+    ```
