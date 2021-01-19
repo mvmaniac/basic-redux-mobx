c5ea885c78f514507c6e4a6cdc9d765c222ed810
yhs <mvmaniaz@gmail.com>
2021-01-19 23:58:32 +0900

    Remove: rename folder

--- 04-mobx-react/src/useStore.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {storeContext} from './Context';
-
-function useUserStore() {
-  const {userStore} = React.useContext(storeContext);
-  return userStore;
-}
-
-export default useUserStore;
